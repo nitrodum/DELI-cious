5200e4b22b8eaecf61ec03cda82818b2ce644306
Nima_Lama <nitrodum@yahoo.com>
2024-11-07 13:52:35 -0800

    implemented orderScreen to take inputs

--- DELI-ciousApplication/src/main/java/com/pluralsight/UserInterface.java
+++ DELI-ciousApplication/src/main/java/com/pluralsight/UserInterface.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class UserInterface {
     private static final Scanner scanner = new Scanner(System.in);
     private static boolean running = true;
+    private static boolean runningOrder = true;
 
     public static void run() {
         while (running) {
@@ -18,12 +19,37 @@ public class UserInterface {
                 "Welcome to DELI-cous!\n" +
                 "1) New Order\n" +
                 "0) Exit");
-        String choice = scanner.nextLine();
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
 
         switch (choice) {
-            case "1" -> System.out.println("Not implemented");
-            case "0" -> running = false;
+            case 1 -> { while (runningOrder) { orderScreen();} }
+            case 0 -> running = false;
             default -> System.out.println("Invalid Choice");
         }
+    }
+
+    private static void orderScreen() {
+        System.out.println("----------------------------------------------------------------------------------------------------\n" +
+                "What would you like to add to the order?\n" +
+                "1) Add Sandwich\n" +
+                "2) Add Drink\n" +
+                "3) Add Chip\n" +
+                "4) Checkout\n" +
+                "0) Cancel Order");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1 -> System.out.println("Not implemented");
+            case 2 -> System.out.println("Not implemented");
+            case 3 -> System.out.println("Not implemented");
+            case 4 -> System.out.println("Not implemented");
+            case 0 -> runningOrder = false;
+            default -> System.out.println("Invalid Choice");
+        }
+
     }
 }
